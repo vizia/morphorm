f7d0fdfa57082311ec1f74379fd55ca0b3d41b54
George Atkinson <geomyles@yahoo.co.uk>
2022-09-11 01:26:41 +0100

    Add cross_before and cross_after

--- src/node2.rs
+++ src/node2.rs
@@ -24,6 +24,10 @@ where
 
     fn main_after(&self, store: &Self::Store) -> Option<Units>;
 
+    fn cross_before(&self, store: &Self::Store) -> Option<Units>;
+
+    fn cross_after(&self, store: &Self::Store) -> Option<Units>;
+
     fn content_size(&self, store: &Self::Store, cross_size: f32) -> Option<f32>;
 
     // fn first_child<N: Node>(&self, store: &Self::Store, tree: &Self::Tree) -> Option<N>;
