0b7b753b47b359c6ab03d5ccd01eda49036a7764
George Atkinson <geomyles@yahoo.co.uk>
2023-03-15 15:05:49 +0000

    Refactor tests

--- tests/left.rs
+++ tests/left.rs
@@ -236,7 +236,7 @@ fn stretch_left_stretch_width() {
 }
 
 #[test]
-fn pixels_left_self_directed() {
+fn pixels_left_pixels_width_self_directed() {
     let mut world = World::default();
 
     let root = world.add(None);
@@ -263,7 +263,7 @@ fn pixels_left_self_directed() {
 }
 
 #[test]
-fn percentage_left_self_directed() {
+fn percentage_left_pixels_width_self_directed() {
     let mut world = World::default();
 
     let root = world.add(None);
@@ -290,7 +290,7 @@ fn percentage_left_self_directed() {
 }
 
 #[test]
-fn stretch_left_self_directed() {
+fn stretch_left_pixels_width_self_directed() {
     let mut world = World::default();
 
     let root = world.add(None);
@@ -314,4 +314,85 @@ fn stretch_left_self_directed() {
     root.layout(&mut world.cache, &world.tree, &world.store);
 
     assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 500.0, posy: 0.0, width: 100.0, height: 150.0 }));
+}
+
+#[test]
+fn pixels_left_percentage_width_self_directed() {
+    let mut world = World::default();
+
+    let root = world.add(None);
+    world.set_width(root, Units::Pixels(600.0));
+    world.set_height(root, Units::Pixels(600.0));
+
+    world.set_layout_type(root, LayoutType::Row);
+
+    let node = world.add(Some(root));
+    world.set_width(node, Units::Percentage(25.0));
+    world.set_height(node, Units::Pixels(150.0));
+    world.set_left(node, Units::Pixels(50.0));
+    world.set_position_type(node, PositionType::SelfDirected);
+
+    root.layout(&mut world.cache, &world.tree, &world.store);
+
+    assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 50.0, posy: 0.0, width: 150.0, height: 150.0 }));
+
+    world.set_layout_type(root, LayoutType::Column);
+
+    root.layout(&mut world.cache, &world.tree, &world.store);
+
+    assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 50.0, posy: 0.0, width: 150.0, height: 150.0 }));
+}
+
+#[test]
+fn percentage_left_percentage_width_self_directed() {
+    let mut world = World::default();
+
+    let root = world.add(None);
+    world.set_width(root, Units::Pixels(600.0));
+    world.set_height(root, Units::Pixels(600.0));
+
+    world.set_layout_type(root, LayoutType::Row);
+
+    let node = world.add(Some(root));
+    world.set_width(node, Units::Percentage(25.0));
+    world.set_height(node, Units::Pixels(150.0));
+    world.set_left(node, Units::Percentage(50.0));
+    world.set_position_type(node, PositionType::SelfDirected);
+
+    root.layout(&mut world.cache, &world.tree, &world.store);
+
+    assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 300.0, posy: 0.0, width: 150.0, height: 150.0 }));
+
+    world.set_layout_type(root, LayoutType::Column);
+
+    root.layout(&mut world.cache, &world.tree, &world.store);
+
+    assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 300.0, posy: 0.0, width: 150.0, height: 150.0 }));
+}
+
+#[test]
+fn stretch_left_percentage_width_self_directed() {
+    let mut world = World::default();
+
+    let root = world.add(None);
+    world.set_width(root, Units::Pixels(600.0));
+    world.set_height(root, Units::Pixels(600.0));
+
+    world.set_layout_type(root, LayoutType::Row);
+
+    let node = world.add(Some(root));
+    world.set_width(node, Units::Percentage(25.0));
+    world.set_height(node, Units::Pixels(150.0));
+    world.set_left(node, Units::Stretch(1.0));
+    world.set_position_type(node, PositionType::SelfDirected);
+
+    root.layout(&mut world.cache, &world.tree, &world.store);
+
+    assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 450.0, posy: 0.0, width: 150.0, height: 150.0 }));
+
+    world.set_layout_type(root, LayoutType::Column);
+
+    root.layout(&mut world.cache, &world.tree, &world.store);
+
+    assert_eq!(world.cache.bounds(node), Some(&Rect { posx: 450.0, posy: 0.0, width: 150.0, height: 150.0 }));
 }
